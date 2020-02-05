7b4861738d01831b6e96d6e18add34fe750ba490
Dervillay <james.gascoigneburns7@gmail.com>
2020-02-05 16:42:47 +0000

    Add point lighting

--- app/living-room.js
+++ app/living-room.js
@@ -3,20 +3,33 @@
 ///////////////
 
 // Vertex shader program
-var VSHADER_SOURCE = 
+var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
   'attribute vec4 a_Color;\n' +
   'attribute vec4 a_Normal;\n' +
   'uniform mat4 u_MvpMatrix;\n' +
-  'uniform mat4 u_NormalMatrix;\n' +
+  'uniform mat4 u_ModelMatrix;\n' +   // Model matrix
+  'uniform mat4 u_NormalMatrix;\n' +  // Transformation matrix of the normal
+  'uniform vec3 u_LightColor;\n' +    // Light color
+  'uniform vec3 u_LightPosition;\n' + // Position of the light source (in the world coordinate system)
+  'uniform vec3 u_AmbientLight;\n' +  // Ambient light color
   'varying vec4 v_Color;\n' +
   'void main() {\n' +
-  '  vec3 lightDirection = vec3(-0.35, 0.35, 0.87);\n' +
   '  gl_Position = u_MvpMatrix * a_Position;\n' +
+     // Recalculate the normal based on the model matrix and make its length 1.
   '  vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));\n' +
-  '  float nDotL = max(dot(normal, lightDirection), 0.0);\n' +
-  '  v_Color = a_Color;\n' + // use for cube
-  //'  v_Color = vec4(a_Color.rgb * nDotL, a_Color.a);\n' + // Use for OBJ models
+     // Calculate world coordinate of vertex
+  '  vec4 vertexPosition = u_ModelMatrix * a_Position;\n' +
+     // Calculate the light direction and make it 1.0 in length
+  '  vec3 lightDirection = normalize(u_LightPosition - vec3(vertexPosition));\n' +
+     // The dot product of the light direction and the normal
+  '  float nDotL = max(dot(lightDirection, normal), 0.0);\n' +
+     // Calculate the color due to diffuse reflection
+  '  vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
+     // Calculate the color due to ambient reflection
+  '  vec3 ambient = u_AmbientLight * a_Color.rgb;\n' +
+     //  Add the surface colors due to diffuse reflection and ambient reflection
+  '  v_Color = vec4(diffuse + ambient, a_Color.a);\n' + 
   '}\n';
 
 // Fragment shader program
@@ -50,57 +63,97 @@ function main() {
     return;
   }
 
-  // Set the clear color and enable the depth test
-  gl.clearColor(0.2, 0.2, 0.2, 1.0);
-  gl.enable(gl.DEPTH_TEST);
+  // Array of vertices for cube
+  var vertices = new Float32Array([
+    2.0, 2.0, 2.0,  -2.0, 2.0, 2.0,  -2.0,-2.0, 2.0,   2.0,-2.0, 2.0, // v0-v1-v2-v3 front
+    2.0, 2.0, 2.0,   2.0,-2.0, 2.0,   2.0,-2.0,-2.0,   2.0, 2.0,-2.0, // v0-v3-v4-v5 right
+    2.0, 2.0, 2.0,   2.0, 2.0,-2.0,  -2.0, 2.0,-2.0,  -2.0, 2.0, 2.0, // v0-v5-v6-v1 up
+   -2.0, 2.0, 2.0,  -2.0, 2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0,-2.0, 2.0, // v1-v6-v7-v2 left
+   -2.0,-2.0,-2.0,   2.0,-2.0,-2.0,   2.0,-2.0, 2.0,  -2.0,-2.0, 2.0, // v7-v4-v3-v2 down
+    2.0,-2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0, 2.0,-2.0,   2.0, 2.0,-2.0  // v4-v7-v6-v5 back
+  ]);
 
-  // Get the storage locations of attribute and uniform variables
-  var program = gl.program;
-  program.a_Position = gl.getAttribLocation(program, 'a_Position');
-  program.a_Normal = gl.getAttribLocation(program, 'a_Normal');
-  program.a_Color = gl.getAttribLocation(program, 'a_Color');
-  program.u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
-  program.u_NormalMatrix = gl.getUniformLocation(program, 'u_NormalMatrix');
-
-  if (program.a_Position < 0 ||  program.a_Normal < 0 || program.a_Color < 0 ||
-      !program.u_MvpMatrix || !program.u_NormalMatrix) {
-    console.log('Failed to get variable storage location'); 
-    return;
-  }
-
-  // Array of vertices and colours for cube
-  var verticesColors = new Float32Array([
-    // Vertex coordinates and color
-     100.0,  100.0,  100.0,     1.0,  1.0,  1.0,  // v0 White
-    -100.0,  100.0,  100.0,     1.0,  0.0,  1.0,  // v1 Magenta
-    -100.0, -100.0,  100.0,     1.0,  0.0,  0.0,  // v2 Red
-     100.0, -100.0,  100.0,     1.0,  1.0,  0.0,  // v3 Yellow
-     100.0, -100.0, -100.0,     0.0,  1.0,  0.0,  // v4 Green
-     100.0,  100.0, -100.0,     0.0,  1.0,  1.0,  // v5 Cyan
-    -100.0,  100.0, -100.0,     0.0,  0.0,  1.0,  // v6 Blue
-    -100.0, -100.0, -100.0,     0.0,  0.0,  0.0   // v7 Black
+  // Array of colors for cube
+  var colors = new Float32Array([
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v1-v2-v3 front
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v3-v4-v5 right
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v5-v6-v1 up
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v1-v6-v7-v2 left
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v7-v4-v3-v2 down
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0　    // v4-v7-v6-v5 back
   ]);
 
   // Prepare empty buffer objects for vertex coordinates, colors and normals
-  var model = initVertexBuffersCube(gl, verticesColors);
+  var model = initVertexBuffersCube(gl, vertices, colors);
   if (!model) {
     console.log('Failed to set the vertex information');
     return;
   }
 
-  // Calculate view projection matrix
-  var viewProjMatrix = new Matrix4();
-  viewProjMatrix.setPerspective(30, canvas.width/canvas.height, 1, 5000);
-  viewProjMatrix.lookAt(0, 500, 200, 0, 0, 0, 0, 1, 0);
+  // Set the clear color and enable the depth test
+  gl.clearColor(0.2, 0.2, 0.2, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  var program = gl.program;
+
+  // Get the storage locations of attribute variables
+  program.a_Position = gl.getAttribLocation(program, 'a_Position');
+  program.a_Normal = gl.getAttribLocation(program, 'a_Normal');
+  program.a_Color = gl.getAttribLocation(program, 'a_Color');
+
+  // Get the storage locations of uniform variables
+  program.u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
+  program.u_NormalMatrix = gl.getUniformLocation(program, 'u_NormalMatrix');
+  program.u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  program.u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
+  program.u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
+  program.u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
+
+  if (program.a_Position < 0 ||  program.a_Normal < 0 || program.a_Color < 0 ||
+      !program.u_MvpMatrix || !program.u_NormalMatrix || !program.u_ModelMatrix ||
+      !program.u_LightColor || !program.u_LightPosition || !program.u_AmbientLight) {
+    console.log('Failed to get variable storage location'); 
+    return;
+  }
+
+  // Set the light color (white)
+  gl.uniform3f(program.u_LightColor, 1.0, 1.0, 1.0);
+  // Set the light direction (in the world coordinate)
+  gl.uniform3f(program.u_LightPosition, 2.3, 4.0, 3.5);
+  // Set the ambient light
+  gl.uniform3f(program.u_AmbientLight, 0.2, 0.2, 0.2);
+
+  var modelMatrix = new Matrix4();  // Model matrix
+  var mvpMatrix = new Matrix4();    // Model view projection matrix
+  var normalMatrix = new Matrix4(); // Transformation matrix for normals
+
+  // Calculate the model matrix
+  modelMatrix.setRotate(90, 0, 1, 0); // Rotate around the y-axis
+  // Pass the model matrix to u_ModelMatrix
+  gl.uniformMatrix4fv(program.u_ModelMatrix, false, modelMatrix.elements);
+
+  // Pass the model view projection matrix to u_MvpMatrix
+  mvpMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
+  mvpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
+  mvpMatrix.multiply(modelMatrix);
+  gl.uniformMatrix4fv(program.u_MvpMatrix, false, mvpMatrix.elements);
+
+  // Pass the matrix to transform the normal based on the model matrix to u_NormalMatrix
+  normalMatrix.setInverseOf(modelMatrix);
+  normalMatrix.transpose();
+  gl.uniformMatrix4fv(program.u_NormalMatrix, false, normalMatrix.elements);
+
+  // Clear color and depth buffer
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // Read OBJ file
-  //readOBJFile("../models/cube.obj", gl, model, 60, true);
+  //readOBJFile("../models/mug.obj", gl, model, 60, true);
 
   var tick = function() { // Start drawing
-    // draw(gl, gl.program, viewProjMatrix, model);
+    // draw(gl, gl.program, mvpMatrix, model);
     // requestAnimationFrame(tick, canvas);
 
-    drawCube(gl, program, viewProjMatrix, model);
+    gl.drawElements(gl.TRIANGLES, model, gl.UNSIGNED_BYTE, 0);
     requestAnimationFrame(tick, canvas);
   }
   tick();
@@ -109,14 +162,6 @@ function main() {
 /////////////////
 /// FUNCTIONS ///
 /////////////////
-
-function drawCube(gl, program, viewProjMatrix, model) {
-  // Pass the model view projection matrix to u_MvpMatrix
-  gl.uniformMatrix4fv(program.u_MvpMatrix, false, viewProjMatrix.elements);
-  // Clear color and depth buffer
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.drawElements(gl.TRIANGLES, model, gl.UNSIGNED_BYTE, 0);
-}
 
 // Calculate normal
 function calcNormal(p0, p1, p2) {
@@ -155,48 +200,42 @@ function initVertexBuffers(gl, program) {
 }
 
 // Create and initialise a buffer object for cubes
-function initVertexBuffersCube(gl, verticesColors) {
+function initVertexBuffersCube(gl, vertices, colors) {
+
+  // Normals of cube
+  var normals = new Float32Array([
+    0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+    0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+   -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+    0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+    0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+  ]);
 
   // Indices of the vertices
   var indices = new Uint8Array([
     0, 1, 2,   0, 2, 3,    // front
-    0, 3, 4,   0, 4, 5,    // right
-    0, 5, 6,   0, 6, 1,    // up
-    1, 6, 7,   1, 7, 2,    // left
-    7, 4, 3,   7, 3, 2,    // down
-    4, 7, 6,   4, 6, 5     // back
- ]);
+    4, 5, 6,   4, 6, 7,    // right
+    8, 9,10,   8,10,11,    // up
+   12,13,14,  12,14,15,    // left
+   16,17,18,  16,18,19,    // down
+   20,21,22,  20,22,23     // back
+  ]);
 
-  // Create a buffer object
-  var vertexColorBuffer = gl.createBuffer();
-  var indexBuffer = gl.createBuffer();
-  if (!vertexColorBuffer || !indexBuffer) {
-    return -1;
-  }
+  // Write the vertex property to buffers (coordinates, colors and normals)
+  if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
+  if (!initArrayBuffer(gl, 'a_Color', colors, 3, gl.FLOAT)) return -1;
+  if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
 
-  // Write the vertex coordinates and color to the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
-
-  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
-  // Assign the buffer object to a_Position and enable the assignment
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if(a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
-  gl.enableVertexAttribArray(a_Position);
-  // Assign the buffer object to a_Color and enable the assignment
-  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  if(a_Color < 0) {
-    console.log('Failed to get the storage location of a_Color');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-  gl.enableVertexAttribArray(a_Color);
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   // Write the indices to the buffer object
+  var indexBuffer = gl.createBuffer();
+  if (!indexBuffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
@@ -217,6 +256,29 @@ function createEmptyArrayBuffer(gl, a_attribute, num, type) {
   gl.enableVertexAttribArray(a_attribute); // Enable the assignment
 
   return buffer;
+}
+
+function initArrayBuffer(gl, attribute, data, num, type) {
+  // Create a buffer object
+  var buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  // Assign the buffer object to the attribute variable
+  var a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment of the buffer object to the attribute variable
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
 }
 
 // Read an OBJ file
